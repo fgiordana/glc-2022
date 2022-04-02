b760911c5886a76f034ac966cc0fa56514c7a561
Francesco Giordana <fgiordana@netflix.com>
2022-04-02 02:00:06 -0700

    Added event loop and camera control

--- 3-render-loop/glc-rs/src/lib.rs
+++ 3-render-loop/glc-rs/src/lib.rs
@@ -2,6 +2,7 @@ mod camera;
 mod scene;
 mod utils;
 
+use bevy::ecs::event::Events;
 use bevy::prelude::*;
 use wasm_bindgen::{prelude::*, JsCast};
 
@@ -20,6 +21,7 @@ pub fn init() {
 #[wasm_bindgen]
 pub struct Glc {
     app: App,
+    camera_events: Vec<camera::CameraMoveEvent>,
 }
 
 #[wasm_bindgen]
@@ -33,25 +35,49 @@ impl Glc {
 
         let mut app = App::new();
         app.insert_resource(WindowDescriptor {
-                #[cfg(target_arch = "wasm32")]
-                canvas: Some(format!("#{canvas_id}")),
-                decorations: false,
-                width: canvas.width() as f32,
-                height: canvas.height() as f32,
-                ..Default::default()
-            })
-            .insert_resource(AmbientLight {
-                brightness: 0.1,
-                color: Color::WHITE,
-            })
-            .add_plugins(DefaultPlugins)
-            .add_startup_system(scene::create_scene)
-            .add_startup_system(camera::create_camera)
-            .update();
-        
+            #[cfg(target_arch = "wasm32")]
+            canvas: Some(format!("#{canvas_id}")),
+            decorations: false,
+            width: canvas.width() as f32,
+            height: canvas.height() as f32,
+            ..Default::default()
+        })
+        .insert_resource(AmbientLight {
+            brightness: 0.5,
+            color: Color::WHITE,
+        })
+        .add_plugins(DefaultPlugins)
+        .add_event::<camera::CameraMoveEvent>()
+        .add_startup_system(scene::create_scene)
+        .add_startup_system(camera::create_camera)
+        .add_system(scene::animate)
+        .add_system(camera::move_camera)
+        .update();
+
         Self {
-            app
+            app,
+            camera_events: vec![],
         }
     }
 
+    pub fn update(&mut self) {
+        self.app.update();
+
+        let mut events = self
+            .app
+            .world
+            .get_resource_mut::<Events<camera::CameraMoveEvent>>()
+            .unwrap();
+        for evt in self.camera_events.iter() {
+            events.send(evt.clone());
+        }
+        self.camera_events.clear();
+    }
+
+    pub fn move_camera(&mut self, rx: f32, ry: f32, z: f32) {
+        self.camera_events.push(camera::CameraMoveEvent {
+            rotate: Vec2::new(rx, ry),
+            zoom: z,
+        });
+    }
 }
