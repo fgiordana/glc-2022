4b34e6afa22bcc4678642b2556a742d33e122360
Francesco Giordana <fgiordana@netflix.com>
2022-04-01 22:28:09 -0700

    created React Viewer and bevy app to render to Viewer canvas

--- 2_render_to_canvas/glc-rs/src/lib.rs
+++ 2_render_to_canvas/glc-rs/src/lib.rs
@@ -1,7 +1,9 @@
+mod camera;
+mod scene;
 mod utils;
 
 use bevy::prelude::*;
-use wasm_bindgen::prelude::*;
+use wasm_bindgen::{prelude::*, JsCast};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -20,8 +22,36 @@ pub struct Glc {
     app: App,
 }
 
+#[wasm_bindgen]
 impl Glc {
-    pub fn new(canvas_id: &str) {
+    pub fn new(canvas_id: &str) -> Self {
         utils::log(&format!("Creating new GLC app with canvas id: {canvas_id}"));
+
+        let document = web_sys::window().unwrap().document().unwrap();
+        let canvas = document.get_element_by_id(canvas_id).unwrap();
+        let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into().unwrap();
+
+        let mut app = App::new();
+        app.insert_resource(WindowDescriptor {
+                #[cfg(target_arch = "wasm32")]
+                canvas: Some(format!("#{canvas_id}")),
+                decorations: false,
+                width: canvas.width() as f32,
+                height: canvas.height() as f32,
+                ..Default::default()
+            })
+            .insert_resource(AmbientLight {
+                brightness: 0.1,
+                color: Color::WHITE,
+            })
+            .add_plugins(DefaultPlugins)
+            .add_startup_system(scene::create_scene)
+            .add_startup_system(camera::create_camera)
+            .update();
+        
+        Self {
+            app
+        }
     }
+
 }
--- 2_render_to_canvas/glc-ui/src/App.js
+++ 2_render_to_canvas/glc-ui/src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
 import CssBaseline from '@mui/material/CssBaseline';
 import { Container } from "@mui/material";
-import { init } from 'glc-wasm';
+import Viewer from "./Viewer";
+import { Glc, init } from 'glc-wasm';
 
 init();
 
 export default function App() {
+    const glcRef = React.useRef();
+
+    React.useEffect(() => {
+        glcRef.current = Glc.new("glc-canvas");
+    })
+
     return (
         <React.Fragment>
             <CssBaseline>
                 <Container>
-                    Hello World!
+                    <Viewer canvasId='glc-canvas' />
                 </Container>
             </CssBaseline>
         </React.Fragment>
